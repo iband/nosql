08799c37c71d5e7c5d3a4d182738ea0d4e98d20d
Vitaly Ishkulov <vit@iva-soft.com>
2014-03-21 23:06:10 +0600

    Task #1. Added db scripts and edited MessageRepository

---
+++ SQL/dbo.Likes.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[likes] (
+    [userName]   VARCHAR (100)    NOT NULL,
+    [messageId]  UNIQUEIDENTIFIER NULL,
+    [createDate] DATETIME         NULL,
+    PRIMARY KEY CLUSTERED ([userName] ASC),
+    CONSTRAINT [FK_Likes_to_Messages] FOREIGN KEY ([messageId]) REFERENCES [dbo].[messages] ([Id])
+);
---
+++ SQL/dbo.Messages.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].messages
+(
+	[Id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY, 
+    [userName] VARCHAR(100) NULL, 
+    [text] VARCHAR(1000) NULL, 
+    [createDate] DATETIME NULL, 
+    [version] ROWVERSION NOT NULL
+
+)
